077659a8b2fd45ed651fe0f93f905230eb0d5fde
Armen <armengabrielyan16@gmail.com>
2021-02-10 23:07:34 +0400

    feat: add problem2

---
+++ week1/problem2.py
@@ -0,0 +1,8 @@
+text = input('Enter text: ')
+start_index = int(input('Enter start index: '))
+end_index = int(input('Enter end index: '))
+
+print('The given text:', text)
+print('Start index:', start_index)
+print('End index:', end_index)
+print('Output string:', text[start_index:end_index])
